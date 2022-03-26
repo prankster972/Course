d8d02fac39859dc5a42d7923885d0d4c332ab1f5
prankster972 <morozovroman.ist@gmail.com>
2022-03-26 19:28:44 +0400

    "main"

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,14 @@ Route::get('/sign-up', function () {
 });
 Route::post('/login/submit', 'LoginController@submit') -> name('login-sub');
 Route::get('/login/submit', 'LoginController@submit') -> name('login-sub');
+
 Route::post('/main/event', 'EventController@event') -> name('event-ev');
 Route::get('/main/event', 'EventController@event') -> name('event-ev');
+
 Route::get('/main/event/list', 'EventController@event2') -> name('event-list');
 Route::put('/main/event/list/', 'EventController@event2') -> name('event-list');
 Route::post('/main/event/list', 'EventController@event2') -> name('event-list');
+
 Route::get('/main/event/list1/', 'EventController@MRList') -> name('event-list-id');
 
 
